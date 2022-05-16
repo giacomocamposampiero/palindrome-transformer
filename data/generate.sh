7ccf7e1547bd95bba90bdb448aa53cfad8e8e127
franznowak <franz.g.nowak@gmail.com>
2022-05-16 14:49:34 +0200

    add execute permission to data generation bash script. Add data generation for parity and palindrome to generation bash script.

--- data/generate.sh
+++ data/generate.sh
@@ -5,3 +5,15 @@ python data_generator.py --data_type "first" --train_size 10000 --test_size 2000
 python data_generator.py --data_type "first" --train_size 10000 --train_len 30
 python data_generator.py --data_type "first" --train_size 10000 --train_len 100
 python data_generator.py --data_type "first" --train_size 10000 --train_len 300
+
+# PARITY learning experiments
+python data_generator.py --data_type "parity" --train_size 10000 --test_size 2000 --train_len 10 --test_len 1000                       
+python data_generator.py --data_type "parity" --train_size 10000 --train_len 30
+python data_generator.py --data_type "parity" --train_size 10000 --train_len 100
+python data_generator.py --data_type "parity" --train_size 10000 --train_len 300
+
+# PALINDROME learning experiments
+python data_generator.py --data_type "palindrome" --train_size 10000 --test_size 2000 --train_len 10 --test_len 1000                       
+python data_generator.py --data_type "palindrome" --train_size 10000 --train_len 30
+python data_generator.py --data_type "palindrome" --train_size 10000 --train_len 100
+python data_generator.py --data_type "palindrome" --train_size 10000 --train_len 300
