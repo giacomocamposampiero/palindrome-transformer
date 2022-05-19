e3508ce104f38be01cc2be8cfa74776e96478b52
franznowak <franz.g.nowak@gmail.com>
2022-05-19 13:01:07 +0200

    add first exact encoder

--- src/encoder.py
+++ src/encoder.py
@@ -71,3 +71,76 @@ class ScaledTransformerEncoderLayer(TransformerEncoderLayer):
                            key_padding_mask=key_padding_mask)
         return self.dropout1(x)
         
+class FirstExactEncoder(TransformerEncoderLayer):
+
+    def __init__(self):
+        torch.nn.Module.__init__(self)
+
+        self.layers = torch.nn.ModuleList([
+            self.FirstLayer(),
+            self.SecondLayer(),
+        ])
+        self.num_layers = len(self.layers)
+        self.norm = None
+    
+    class FirstLayer(TransformerEncoderLayer):
+        def __init__(self):
+            EMBED_DIM = 6
+            W_F1 = [[0, 1, 0, 1, 0, 0]]
+            b_F1 = [-1]
+            W_F2 = [[0], [0], [0], [0], [1], [0]]
+            b_F2 = [0] * EMBED_DIM
+
+            super().__init__(d_model=EMBED_DIM, nhead=1, dim_feedforward=1, dropout=0.)
+
+            # q, k, v all have the same embedding dimension, so we just have to set it once as in_proj
+            self.self_attn.in_proj_weight = torch.nn.Parameter(torch.zeros(3 * EMBED_DIM, EMBED_DIM))
+            self.self_attn.in_proj_bias = torch.nn.Parameter(torch.zeros(3 * EMBED_DIM))
+
+            # out_proj weights and bias
+            self.self_attn.out_proj.weight = torch.nn.Parameter(torch.zeros(EMBED_DIM, EMBED_DIM))
+            self.self_attn.out_proj.bias = torch.nn.Parameter(torch.zeros(EMBED_DIM))
+
+            # First FFNN
+            self.linear1.weight = torch.nn.Parameter(torch.tensor(W_F1, dtype=torch.float))
+            self.linear1.bias = torch.nn.Parameter(torch.tensor(b_F1, dtype=torch.float))
+
+            # Second FFNN
+            self.linear2.weight = torch.nn.Parameter(torch.tensor(W_F2, dtype=torch.float))
+            self.linear2.bias = torch.nn.Parameter(torch.tensor(b_F2, dtype=torch.float))
+
+        def forward(self, src, src_mask=None, src_key_padding_mask=None):
+            src2 = self.self_attn(src, src, src, attn_mask=src_mask,
+                                key_padding_mask=src_key_padding_mask)[0]
+            src += self.dropout1(src2)
+            src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
+            src += self.dropout2(src2)
+            return src
+
+    class SecondLayer(TransformerEncoderLayer):
+        
+        def __init__(self):
+            EMBED_DIM = 6
+            W_Q = [[0, 0, 1.0, 0, 0, 0]] + [[0] * EMBED_DIM] * 5
+            W_K = [[0, 0, 0, 1, 0, 0]] + [[0] * EMBED_DIM] * 5
+            W_V = [[0] * EMBED_DIM] * 5 + [[0, 0, 0, -0.5, 1, 0]]
+            W_O = [[0] * EMBED_DIM] * 5 + [[0, 0, 0, 0, 0, 1]]
+
+            super().__init__(d_model=EMBED_DIM, nhead=1, dim_feedforward=1, dropout=0.0)
+
+            self.self_attn.in_proj_weight = torch.nn.Parameter(torch.tensor(
+                W_Q +
+                W_K +
+                W_V, dtype=torch.float))
+
+            self.self_attn.in_proj_bias = torch.nn.Parameter(torch.zeros(3 * EMBED_DIM))
+
+            self.self_attn.out_proj.weight = torch.nn.Parameter(torch.tensor(W_O, dtype=torch.float))
+            self.self_attn.out_proj.bias = torch.nn.Parameter(torch.zeros(EMBED_DIM))
+
+            self.linear1.weight = torch.nn.Parameter(torch.zeros(1,EMBED_DIM))
+            self.linear1.bias = torch.nn.Parameter(torch.zeros(1))
+            self.linear2.weight = torch.nn.Parameter(torch.zeros(EMBED_DIM,1))
+            self.linear2.bias = torch.nn.Parameter(torch.zeros(EMBED_DIM))
+
+        forward = super.FirstLayer.forward
