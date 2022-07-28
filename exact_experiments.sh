cbebc7025aaa55d9d928c9c85b22b0eb992855ea
Bernhard Hilmarsson <bennilinn@gmail.com>
2022-07-28 15:46:33 +0200

    File which runs specific experiments for exact solutions

---
+++ exact_experiments.sh
@@ -0,0 +1,20 @@
+
+for test_length in 10 20 40 80 160 320 640 1000 2000 4000 10000
+do
+	python3 exact_experiments.py --lan first --test_length $test_length --size 500 
+done
+
+for test_length in 10 20 40 80 160 320 640 1000 2000 4000 10000
+do
+	python3 exact_experiments.py --lan parity --test_length $test_length --size 500 
+done
+
+for test_length in 10 20 40 80 160 320 640 1000 2000 4000 10000
+do
+	python3 exact_experiments.py --lan one --test_length $test_length --size 500 
+done
+
+for test_length in 2 4 8 16 32 40 50 60 70 80 90 100
+do
+	python3 exact_experiments.py --lan palindrome --test_length $test_length --size 500 
+done
